839b392c12c1cef0b7ea2a97dadbd6a429484dd0
Arma1381 <107887071+Arma1381@users.noreply.github.com>
2022-07-01 16:53:46 +0430

    Update setingFinalEdition.java

--- setingFinalEdition.java
+++ setingFinalEdition.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-/*
+
  
 public class setingFinalEdition extends javax.swing.JFrame {
     String userg="";
